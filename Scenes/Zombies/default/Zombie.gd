5ddec6756effe571d9c2a72fbaa71f77cb476bff
Henrique <henfinzim@gmail.com>
2020-05-24 13:09:56 -0300

    tentando arrumar o bug

--- Scenes/Zombies/default/Zombie.gd
+++ Scenes/Zombies/default/Zombie.gd
@@ -5,44 +5,29 @@ const MOVE_SPEED = 200
 onready var raycast = $RayCast2D
  
 var player = null
-var dir = Vector2()
+var obj = Vector2()
  
 func _ready():
 	add_to_group("zombies")
-	$Area2D.connect("body_entered", self, "attack") 
+	$Sprite/Area2D.connect("body_entered", self, "attack") 
 
 
-func _physics_process(delta):
+func _process(delta):
 	if player == null:
 		return
 	
-	chase_target()
-	rotation = dir.angle()
-	move_and_slide(dir*MOVE_SPEED)
- 
-func chase_target():
-	if(player != null):
-		var look = get_node("RayCast2D")
-		look.cast_to = (player.position - position).rotated(-rotation)
-		look.force_raycast_update()
-		$Sprite2.position = look.cast_to
+	var l_time = 0
+	var look = get_node("RayCast2D")
+	look.cast_to = (player.position-position)
+	look.force_raycast_update()
+	if look.is_colliding() and look.get_collider() == player:
+		obj = look.cast_to
 	
-		# if we can see the target, chase it
-		if not look.is_colliding():
-			print("ta vendo o target")
-			dir = (player.position - position).normalized()
-	
-	  # or chase first scent we can see
-		else:
-			print("nao ta vendo o player")
-			for scent in player.scent_trail:
-				look.cast_to = (scent.position - position).rotated(-rotation)
-				look.force_raycast_update()
-			
-				if not look.is_colliding():
-					dir = (scent.position - position).normalized()
-					break
-			
+	$Sprite.rotation = obj.angle()
+	$icon.position = obj
+	print(obj)
+	move_and_slide(obj.normalized()*MOVE_SPEED)
+
 func attack(body):
 	if(body == player):
 		body.kill()
